6764795363047ac620ac6bb30398a48c75f0c09d
Jacob Barrieault <jacob.barrieault@gmail.com>
2024-09-20 08:21:42 -0400

    Build and serialize some users to users.avro file. Mostly following just along with the Avro getting started tutorial still

--- java-writer/src/main/java/com/github/jbarrieault/App.java
+++ java-writer/src/main/java/com/github/jbarrieault/App.java
@@ -1,11 +1,34 @@
 package com.github.jbarrieault;
 
+import avro.codegen.User;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.avro.file.DataFileWriter;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
+
+        User user = new User();
+        user.setFirstName("Jack");
+        user.setLastName("White");
+        user.setAge(49);
+
+        User user2 = new User("Meg", "White", 49);
+
+        // System.out.println(user);
+
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+        DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+        dataFileWriter.create(user.getSchema(), new File("users.avro"));
+        dataFileWriter.append(user);
+        dataFileWriter.append(user2);
+        dataFileWriter.close();
     }
 }
